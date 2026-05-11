c4007e519a1f9eba7a61076d11993e5f6d45bb94
Francisco Jorquera <fjorquera@uc.cl>
2026-05-10 23:01:10 -0400

    fix: rutas relativas para GitHub Pages e íconos faltantes

--- app.js
+++ app.js
@@ -386,7 +386,7 @@ updateOnline();
 let swRegistration = null;
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(r => {
         swRegistration = r;
         console.log('SW registrado:', r.scope);
--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 (function (root) {
   root.DIVIDAPP_FIREBASE_CONFIG = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
-  };
+  apiKey: "AIzaSyBxKpCLuns8GdlnPTaVtTuRRRyexzyd4mA",
+  authDomain: "t4-g9-5bade.firebaseapp.com",
+  projectId: "t4-g9-5bade",
+  storageBucket: "t4-g9-5bade.firebasestorage.app",
+  messagingSenderId: "173799200957",
+  appId: "1:173799200957:web:b0e8176a9346945beaffd4",
+  measurementId: "G-N1L1BQ50CJ"
+};
 
-  root.DIVIDAPP_FIREBASE_VAPID_KEY = '';
+  root.DIVIDAPP_FIREBASE_VAPID_KEY = 'BDAtGFKzFdtstDvmC1x1IMGpIpaNz1auDVQbzR7VW02iNTOhOYpHtS5psDmlXuYxlxa3JBMctRbtWBPJbNaDlQ8';
 })(typeof self !== 'undefined' ? self : window);
